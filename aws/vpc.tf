237ecb6cfda6f57dfade34a5b13113ec5c4f19eb
Tanner Lake <tanner.lake@gmail.com>
2021-08-22 12:27:20 -0700

    add default subnets and tlake-infra vpc

--- aws/vpc.tf
+++ aws/vpc.tf
@@ -7,3 +7,26 @@ resource "aws_default_vpc" "default" {
     Name = "Default VPC"
   }
 }
+
+resource "aws_default_subnet" "defaults" {
+  for_each = toset([
+    "us-west-2a",
+    "us-west-2b",
+    "us-west-2c",
+    "us-west-2d",
+  ])
+
+  availability_zone = each.key
+}
+
+################
+# tlake-infra
+################
+
+resource "aws_vpc" "tlake_infra" {
+  cidr_block = "10.100.0.0/16"
+
+  tags = {
+    Name = "tlake-infra"
+  }
+}
