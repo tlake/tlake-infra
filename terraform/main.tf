b78a062513937eed0a1c7fd8184d74a0d503d0e2
Tanner Lake <tanner.lake@gmail.com>
2021-08-22 01:40:43 -0700

    delete s3 tf state resources in transition to remote backend

--- terraform/main.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    encrypt        = true
-    region         = "us-west-2"
-    bucket         = "tlake-terraform-remote-state-storage-s3"
-    key            = "terraform-remote-state/terraform.tfstate"
-    dynamodb_table = "tlake-terraform-state-lock-dynamo"
-  }
-}
