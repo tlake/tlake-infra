5f4c0a4ff06e0e31ea515e14ab61a3e63d63a850
Tanner Lake <tanner.lake@gmail.com>
2021-08-22 19:57:26 -0700

    add codefellows ssh sg

--- aws/vpc.tf
+++ aws/vpc.tf
@@ -27,7 +27,7 @@ resource "aws_default_security_group" "default" {
   vpc_id = aws_default_vpc.default.id
 
   tags = {
-    Name   = "Default SG for Default VPC"
+    Name = "Default SG for Default VPC"
   }
 }
 
@@ -41,4 +41,74 @@ resource "aws_vpc" "tlake_infra" {
   tags = {
     Name = "tlake-infra"
   }
+}
+
+################
+# old codefellows
+################
+
+resource "aws_security_group" "codefellows_ssh" {
+  name        = "ssh-access"
+  description = "Security Group created for Code Fellows"
+
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 443
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 443
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 5432
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 5432
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 80
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 80
+    },
+  ]
 }
