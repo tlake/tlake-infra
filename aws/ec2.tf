ce5fc4dbdb2464dcc1f124860fff6fd878b47f45
Tanner Lake <tanner.lake@gmail.com>
2021-08-22 22:58:12 -0700

    import tlake-general ec2 instance

---
+++ aws/ec2.tf
@@ -0,0 +1,14 @@
+################
+# tlake-general
+################
+
+resource "aws_instance" "tlake_general" {
+  ami                  = "ami-0e34e7b9ca0ace12d"
+  iam_instance_profile = "ec2-ssm-role"
+  instance_type        = "t2.micro"
+  security_groups      = [aws_security_group.tlake_default.name]
+
+  tags = {
+    Name = "tlake-general"
+  }
+}
