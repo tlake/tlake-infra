0a53e515be721460170c77bcfca2364a05ceb23c
Tanner Lake <tanner.lake@gmail.com>
2021-08-22 22:36:54 -0700

    import tlake-default and tlake-infra default SGs

--- aws/vpc.tf
+++ aws/vpc.tf
@@ -31,6 +31,62 @@ resource "aws_default_security_group" "default" {
   }
 }
 
+resource "aws_security_group" "tlake_default" {
+  description = "allows http, https, and ssh"
+  name        = "tlake-default"
+  vpc_id      = aws_default_vpc.default.id
+
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    },
+  ]
+
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 443
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 443
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 80
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 80
+    },
+  ]
+}
+
 ################
 # tlake-infra
 ################
@@ -41,6 +97,40 @@ resource "aws_vpc" "tlake_infra" {
   tags = {
     Name = "tlake-infra"
   }
+}
+
+resource "aws_security_group" "tlake_infra_default" {
+  description = "default VPC security group"
+  name        = "default"
+  vpc_id      = aws_vpc.tlake_infra.id
+
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    },
+  ]
+
+  ingress = [
+    {
+      cidr_blocks      = []
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = true
+      to_port          = 0
+    },
+  ]
 }
 
 ################
